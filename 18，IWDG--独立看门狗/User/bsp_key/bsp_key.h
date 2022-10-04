a69c108a9fb196a4185d431cdaafe1f5917a8e00
lolita <mylolitahouse@outlook.com>
2022-10-05 00:37:46 +0800

    add dog

---
+++ 18，IWDG--独立看门狗/User/bsp_key/bsp_key.h
@@ -0,0 +1,25 @@
+#ifndef __KEY_H
+#define __KEY_H
+
+#include "stm32f10x.h"
+
+//  引脚定义
+#define    KEY1_GPIO_CLK     RCC_APB2Periph_GPIOA
+#define    KEY1_GPIO_PORT    GPIOA			   
+#define    KEY1_GPIO_PIN		 GPIO_Pin_0
+
+#define    KEY2_GPIO_CLK     RCC_APB2Periph_GPIOC
+#define    KEY2_GPIO_PORT    GPIOC		   
+#define    KEY2_GPIO_PIN		 GPIO_Pin_13
+
+/* 按键按下为1 */
+#define KEY_ON		1
+#define KEY_OFF		0
+
+/* 函数声明 */
+uint8_t Key_Scan(GPIO_TypeDef* GPIOx,uint16_t GPIO_Pin);
+void Key_GPIO_Config(void);
+
+
+#endif /*__KEY_H*/
+
---
+++ 19，WWDG--窗口看门狗/User/bsp_wwdg/bsp_wwdg.c
@@ -0,0 +1,49 @@
+ 
+#include "bsp_wwdg.h"   
+
+
+
+/* WWDG 中断优先级初始化 */
+static void WWDG_NVIC_Config(void)
+{
+  NVIC_InitTypeDef NVIC_InitStructure; 
+  
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1); 
+  NVIC_InitStructure.NVIC_IRQChannel = WWDG_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+}
+
+/* WWDG 配置函数
+ * tr ：递减计时器的值， 取值范围为：0x7f~0x40
+ * wr ：窗口值，取值范围为：0x7f~0x40
+ * prv：预分频器值，取值可以是
+ *      @arg WWDG_Prescaler_1: WWDG counter clock = (PCLK1(36MHZ)/4096)/1
+ *      @arg WWDG_Prescaler_2: WWDG counter clock = (PCLK1(36mhz)/4096)/2
+ *      @arg WWDG_Prescaler_4: WWDG counter clock = (PCLK1(36mhz)/4096)/4
+ *      @arg WWDG_Prescaler_8: WWDG counter clock = (PCLK1(36mhz)/4096)/8
+ */
+void WWDG_Config(uint8_t tr, uint8_t wr, uint32_t prv)
+{		
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_WWDG, ENABLE);	// 开启 WWDG 时钟	
+	
+	WWDG_SetCounter( tr );																// 设置递减计数器的值		
+	WWDG_SetPrescaler( prv );															// 设置预分频器的值		
+	WWDG_SetWindowValue( wr );														// 设置上窗口值		
+	WWDG_Enable(WWDG_CNT);																// 设置计数器的值，使能WWDG	
+	
+	WWDG_ClearFlag();																			// 清除提前唤醒中断标志位	
+	WWDG_NVIC_Config();																		// 配置WWDG中断优先级	
+	WWDG_EnableIT();																			// 开WWDG 中断
+}
+
+// 喂狗
+void WWDG_Feed(void)
+{	
+	WWDG_SetCounter( WWDG_CNT );													// 喂狗，刷新递减计数器的值，设置成最大WDG_CNT=0X7F
+}
+
+
+/*********************************************END OF FILE**********************/
